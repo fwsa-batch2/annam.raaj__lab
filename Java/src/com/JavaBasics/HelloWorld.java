aa33fad1f1da5f756eaa0db8b8c8af3ecc6090ec
PavanRaaj <pavanraajgoud@gmail.com>
2022-04-11 16:25:48 +0530

    Added Java

---
+++ Java/src/com/JavaBasics/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.JavaBasics;
+
+public class HelloWorld {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hi I am Pavan");
+	    System.out.println("Hello World! How are you?");
+    }
+}

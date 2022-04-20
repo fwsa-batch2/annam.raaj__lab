1861affb990ca88a0f5569520b8df08a9d58c5da
PavanRaaj <pavanraajgoud@gmail.com>
2022-04-20 17:37:22 +0530

    Added Java Exercises.

---
+++ Java Labworks/Basic Exercises/Armstrong.java
@@ -0,0 +1,23 @@
+package com.JavaBasics;
+
+public class Armstrong {
+    public static void main(String[] args){
+        int number = 371 , remainder , result , originalNumber;
+        originalNumber = number;
+        result = 0;
+        while (originalNumber != 0 ){
+            remainder = originalNumber%10;
+            result += Math.pow(remainder,3);
+            originalNumber /= 10;
+
+        }
+
+        if (result == number){
+            System.out.println(number + " is Armstrong Number.");
+        }
+
+        else {
+            System.out.println(number + " is not Armstrong Number.");
+        }
+    }
+}
---
+++ Java Labworks/Basic Exercises/OddorEven.java
@@ -0,0 +1,15 @@
+package com.JavaBasics;
+
+public class OddorEven {
+
+    public static void main(String args[]){
+
+        int a=25;
+        if(a%2==0){
+            System.out.println("Even");
+        }
+      else {
+          System.out.println("Odd");
+        }
+    }
+}
